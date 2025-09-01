e0796600f8b2bb722b9c8c85c5287ef4ce15b27c
Florencia <flormartini11@gmail.com>
2025-08-31 22:29:35 -0300

    Agrego Ejercicio clase3 python de Seleccion Argentina

---
+++ Python/Clase3/EjercicioSeleccion.py
@@ -0,0 +1,9 @@
+seleccionArgentina = {
+    10: {'Nombre': 'Lionel Messi', 'Edad': 38, 'Altura': 1.70, 'Precio': '20 millones de euros', 'Posicion': 'delantero'},
+     5: {'Nombre': 'Leandro Paredes', 'Edad': 31, 'Altura': 1.82, 'Precio': '5 millones de euros', 'Posicion': 'centrocampista'},
+    23: {'Nombre': 'Emiliano Martínez', 'Edad': 32, 'Altura': 1.95, 'Precio': '40 millones de euros', 'Posiciom': 'arquero'},
+     3: {'Nombre': 'Nicolas Tagliafico', 'Edad': 33 , 'Altura': 1.72, 'Precio': '7 millones de euros', 'Posicion': 'defenso'},
+    17: {'Nombre': 'Giuliano Simeone', 'Edad': 22, 'Altura': 1.73, 'Precio': '33 millones de euros', 'Posicion': 'extremo derecho'},
+}
+for valor in seleccionArgentina.values():
+    print(valor)
