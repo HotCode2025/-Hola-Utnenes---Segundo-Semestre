93c1eafaebfd354256dedad89077e18e71adb863
Florencia <flormartini11@gmail.com>
2025-09-08 00:39:02 -0300

    Agrego a la clase 3 argumentos, parametros y la funcion hoinsting

--- JavaScript/SSLeccion03/03-01-funciones.js
+++ JavaScript/SSLeccion03/03-01-funciones.js
@@ -36,3 +36,25 @@ const sumarFuncionFlecha = (a, b) => a + b;
 resultado = sumarFuncionFlecha(3, 7);//Asignamos el valor a una variable
 console.log(resultado);
 
+//Funcion tipo expresion
+let sumar = function(a = 4, b = 8){
+    console.log(arguments[0]); //Muestra el parametro de: a
+    console.log(arguments[1]);//Muestra el parametro de: b
+    console.log(arguments[2]);
+    return a + b + arguments[2];
+}
+resultado = sumar(3, 2, 9);
+console.log(resultado);
+
+//Sumar todos los argumentos
+let respuesta = sumarTodo(5, 4, 13, 10, 9);
+console.log(respuesta);
+function sumarTodo(){
+    let suma = 0;
+    for (let i = 0; i < arguments.length; i++){
+        suma += arguments[i]; //arguments es para arreglos
+    }
+    return suma;
+}
+
+
