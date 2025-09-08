8a4cb45d23d7e8ea32fb889a21db53b8664a062b
Florencia <flormartini11@gmail.com>
2025-09-08 00:21:08 -0300

    Modifico Leccion3 de JS y agrego funciones de tipo self e invoking y los tipos de datos de una funcion

--- JavaScript/SSLeccion03/03-01-funciones.js
+++ JavaScript/SSLeccion03/03-01-funciones.js
@@ -15,3 +15,18 @@ console.log(resultado);
 let x = function(a, b){return a + b}; //necesita cierre con punto y coma
 resultado = x(5, 6); // al llamarla se pone la variable y parentesis
 console.log(resultado);
+
+//Funciones de tipo self e invoking
+(function(a, b){
+    console.log('Ejecutando la función: '+ (a + b));
+})(9, 6);
+
+console.log(typeof miFuncion);
+function miFuncionDos(a, b){
+    console.log(arguments.length);
+}
+miFuncionDos(5, 7, 3, 6);
+
+//toString
+var miFuncionTexto = miFuncionDos.toString();
+console.log(miFuncionTexto);
