b3a96e65b44a95740205433d1133fa79ed9ace4f
Florencia <flormartini11@gmail.com>
2025-09-07 23:31:20 -0300

    Agrego a la clase4 el ejercicio 6 de la tabla de multiplicar

---
+++ Python/Clase4/Ejercicio6.py
@@ -0,0 +1,9 @@
+#Ejercicio 6: Tabla de multiplicar
+#Hacer un programa que pida un número por teclado y guarde
+#en una lista su tabla de multiplicar hasta el 10. Por ejemplo
+#si digita el 5 la lista tendrá: 5, 10, 15, 20, 25, 30, 35, 40, 45, 50
+
+numero = int(input('Ingrese un número: '))
+tabla = [numero * i for i in range(1, 11)] #la lista para la tabla de multiplica
+
+print(f'La tabla de multiplicar del {numero} es: {tabla}')
