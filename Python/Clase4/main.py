e42ce8c6ad25facca7aa1cf6fedef9ba39f91863
Florencia <flormartini11@gmail.com>
2025-09-07 23:32:06 -0300

    Agrego a la clase 4, clase de funciones en python

---
+++ Python/Clase4/main.py
@@ -0,0 +1,10 @@
+#Funciones en Python
+#mi_funcion()No se puede llamar antes de definira una función
+#Definimos una función
+
+def mi_funcion():#Para identificar a la función utilizamos paréntesis
+    print('Saludos a todos los alumnos de la Tecnicatura')
+
+mi_funcion() #Estamos llamando a la función
+mi_funcion() #Se puede llamar una cantidad n de veces
+
