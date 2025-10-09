247a7fb284c6f2344bf5722c2f7bb36e19592380
Otar Kebadze <OtarKebadze@gmail.com>
2025-10-08 22:16:24 -0300

    tarea clase 9 java terminada

---
+++ Java/Clase9/test/testHerencia.java
@@ -0,0 +1,11 @@
+package Clase9.test;
+
+
+import Clase9.domain.Empleado;
+
+public class testHerencia {
+        public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Marco", 5700);
+        System.out.println("Empleado" + empleado1);
+    }
+}
