afcae3e148a46d1cf39a7b7724328f85e32066c4
Florencia <flormartini11@gmail.com>
2025-10-19 23:40:32 -0300

    Agregamos modificaciones a la clase9 de Python donde realizamos validación de valores y también creamos una clase abstracta (ABC)

--- Python/Clase9/FiguraGeometrica.py
+++ Python/Clase9/FiguraGeometrica.py
@@ -1,21 +1,44 @@
-class FiguraGeometrica:
+from abc import ABC, abstractmethod
+#ABC significa: Abstract Base Class, convierte una clase en abstracta
+
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'Valor erróneo para el ancho: {ancho}')
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self.alto = 0
+            print(f'Valor erróneo para el ato: {alto}')
 
     @property
     def ancho(self):
         return self._ancho
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'valor erróneo ancho: {ancho}')
 
     @property
     def alto(self):
         return self._alto
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'valor erróneo alto: {alto}')
+    @abstractmethod
+    def calcular_area(self):
+        pass
 
     def __str__(self):
         return f'FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
+
+    def _validar_valores(self, valor): #Método encapsulado
+        return True if 0 < valor < 10 else False
