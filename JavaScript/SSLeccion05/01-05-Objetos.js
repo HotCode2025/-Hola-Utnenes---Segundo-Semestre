5c635ec9525618ddfb4eff16be98c38a860e0193
Florencia <flormartini11@gmail.com>
2025-10-20 11:40:09 -0300

    Clase05 de JS: diferentes formas de crear objetos

---
+++ JavaScript/SSLeccion05/01-05-Objetos.js
@@ -0,0 +1,31 @@
+//Diferentes formas de crear objetos
+//caso objeto 1
+let miObjeto = new Object(); //Esta es una opción formal
+//caso objeto 2
+let miObjeto2 = {}; //Esta opción es breve y recomendada
+
+//caso String 1
+let miCadena1 = new String('Hola'); //Sintaxis formal
+//caso String 2
+let miCadena2 = 'Hola'; //Esta es la sintaxis simplificada y recomendada
+
+//caso con números 1
+let miNumero = new Number(1); //Es formal no recomendable
+//caso con números 2
+let miNumero2 = 1; //Sintaxis recomendada
+
+//caso boolean 1
+let miBoolean1 = new Boolean(false); //Formal
+//caso boolean 2
+let miBoolean2 = false; //Sintaxis recomendada
+
+//caso Arreglos 1
+let miArreglo1 = new Array(); //formal
+let miArreglo2 = []; //Sintaxis recomendada
+
+//caso function 1
+let miFunction1 = new function(){}; //Todo despues de new es considerado objeto
+//caso function 2
+let miFunction2 = function(){}; //Notación simplificada y recomendada
+
+//Uso de prototype
